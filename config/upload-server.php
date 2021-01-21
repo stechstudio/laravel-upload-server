8c6bedb81db935258b01deff0b55a231a28694f5
Joseph <joseph@stechstudio.com>
2021-01-21 11:32:05 -0500

    option to auto-log events

--- config/upload-server.php
+++ config/upload-server.php
@@ -5,6 +5,9 @@ return [
     'path'          => env('UPLOAD_SERVER_PATH', 'uploads'),
     'partials_path' => env('UPLOAD_SERVER_PARTIALS_PATH', 'uploads/partials'),
 
+    'log'       => env('UPLOAD_SERVER_LOG', true),
+    'log_level' => env('UPLOAD_SERVER_LOG_LEVEL', 'debug'),
+
     'default'  => 'filepond',
 
     // Server-specific config
--- src/UploadServerServiceProvider.php
+++ src/UploadServerServiceProvider.php
@@ -2,10 +2,26 @@
 
 namespace STS\UploadServer;
 
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use STS\UploadServer\Events\AbstractEvent;
+use STS\UploadServer\Events\ChunkedUploadRetrying;
+use STS\UploadServer\Events\ChunkedUploadStarted;
+use STS\UploadServer\Events\ChunkReceived;
+use STS\UploadServer\Events\UploadComplete;
+use STS\UploadServer\Events\UploadRemoved;
 
 class UploadServerServiceProvider extends ServiceProvider
 {
+    protected $events = [
+        ChunkedUploadStarted::class,
+        ChunkReceived::class,
+        ChunkedUploadRetrying::class,
+        UploadComplete::class,
+        UploadRemoved::class
+    ];
+
     /**
      * Bootstrap the application services.
      */
@@ -13,8 +29,12 @@ class UploadServerServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/upload-server.php' => config_path('upload-server.php'),
+                __DIR__ . '/../config/upload-server.php' => config_path('upload-server.php'),
             ], 'config');
+        }
+
+        if ($this->app['config']->get('upload-server.log')) {
+            $this->logEvents($this->app['config']->get('upload-server.log_level'));
         }
     }
 
@@ -23,6 +43,21 @@ class UploadServerServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/upload-server.php', 'upload-server');
+        $this->mergeConfigFrom(__DIR__ . '/../config/upload-server.php', 'upload-server');
+    }
+
+    protected function logEvents($level)
+    {
+        Event::listen($this->events, function (AbstractEvent $event) use ($level) {
+            $message = ucfirst(str_replace('-', ' ',
+                Str::kebab((new \ReflectionClass($event))->getShortName())
+            ));
+
+            $this->app['log']->log($level, $message, array_merge($event->meta, [
+                'id'      => $event->file->id(),
+                'percent' => $event->step->percentComplete(),
+                'path'    => $event->file->getRealPath()
+            ]));
+        });
     }
 }
